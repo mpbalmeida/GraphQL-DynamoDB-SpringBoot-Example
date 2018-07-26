41c2fb4df1de6450aecc1cee5ffa011734e7d702
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-07-26 19:34:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+graphql-dynamodb-spring-example
---
+++ src/main/java/br/com/faroltech/graphqldynamodbspringexample/GraphqlDynamodbSpringExampleApplication.java
@@ -0,0 +1,12 @@
+package br.com.faroltech.graphqldynamodbspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDynamodbSpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlDynamodbSpringExampleApplication.class, args);
+    }
+}
